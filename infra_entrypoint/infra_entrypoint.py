0f0a095b3e9a56ff48641a339f2a3b8c2ffa3132
Supletsov Dmitriy <supletsovd@gmail.com>
2025-04-19 13:44:37 +0300

    добавлена сборка для ansible control node

--- infra_entrypoint/infra_entrypoint.py
+++ infra_entrypoint/infra_entrypoint.py
@@ -9,6 +9,7 @@ public_ssh_key_name = 'id_rsa.pub'
 private_ssh_key_name = 'id_rsa'
 public_ssh_key_path = os.path.join(ssh_keys_directory, public_ssh_key_name)
 private_ssh_key_path = os.path.join(ssh_keys_directory, private_ssh_key_name)
+ansible_control_node_port = 2221
 debian_port = 2222
 alma_linux_port = 2223
 user = 'root'
@@ -28,18 +29,28 @@ if __name__ == '__main__':
         print("ERROR: Docker демон не запущен. Пожалуйста, запустите Docker Desktop или службу Docker.")
         exit(1)
 
+    print("INFO: Docker демон запущен.")
+
     if not run_docker_compose(path_to_infrastructure):
         print("ERROR: Не удалось запустить сеть Docker.")
         exit(1)
 
+    print("INFO: Запускаем сеть Docker. Это может занять несколько минут.")
+
+    if not wait_for_ssh(host, ansible_control_node_port, 300):
+        print("ERROR: не удалось получить доступ к Ansible Control Node")
+        exit(1)
+
+    print(f"INFO: Ansible Control Node успешно запущен и прослушивает порт {ansible_control_node_port}")
+
     if not wait_for_ssh(host, debian_port, 300):
         print("ERROR: не удалось получить доступ к Debian серверу")
         exit(1)
-    else:
-        print(f"INFO: Debian сервер успешно запущен и прослушивает порт {debian_port}")
+
+    print(f"INFO: Debian сервер успешно запущен и прослушивает порт {debian_port}")
 
     if not wait_for_ssh(host, alma_linux_port, 300):
         print("ERROR: не удалось получить доступ к AlmaLinux серверу")
         exit(1)
-    else:
-        print(f"INFO: AlmaLinux сервер успешно запущен и прослушивает порт {alma_linux_port}")
+
+    print(f"INFO: AlmaLinux сервер успешно запущен и прослушивает порт {alma_linux_port}")
