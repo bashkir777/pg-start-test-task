13574ba57ef4646dcb4afce6fd066073b678890f
Supletsov Dmitriy <supletsovd@gmail.com>
2025-04-20 17:33:09 +0300

    Добавил установку postgres client на нецелевой хост

--- entrypoint/main.py
+++ entrypoint/main.py
@@ -3,5 +3,6 @@ from infra_entrypoint.tools import docker
 
 if __name__ == '__main__':
     infra_entrypoint('../infrastructure/')
+    print("INFO: pg-deployer запущен. Его выполнение может занять несколько минут. Не прерывайте программу.")
     docker.execute_command_in_container("ansible_control_node",
                                         "python3 /pg-deployer/pg_deployer.py debian_ssh almalinux_ssh")
